30cfbbd7d83931d5402494a4ac1d0a5383e8fc11
Shogun4466 <35836530+Shogun4466@users.noreply.github.com>
2018-02-14 15:46:52 +0100

    Update Oppg-3.go

--- Oblig-1/Oppg-3.go
+++ Oblig-1/Oppg-3.go
@@ -1,14 +1,29 @@
-package main import ( "fmt"
+//I denne koden så opretes det en uendelig loop som blir avslutet når datamaskinen får inputt av type CTRL+fn+f2. Når inputet blir tatt imot,
+//så skriver koden meldingen "You shall not pass SIGINT
+// Når programet Kjører, så skrives det ut en melding "Loooooooop" intil programet stopes. Programet tar ikke opp noe av prossesorkraften 
+// under kjøringen, men det tar 0,9MB. 
+package main 
+//Bruker import for å inhente alle nødvendige funksjoner
+import ( 
+"fmt"
+	
 "os"
 
 "os/signal"
 
 "syscall"
-) func main() { indefLoop() } func indefLoop() { c := make(chan os.Signal, 1)
+) 
+func main() { 
+	indefLoop() 
+} 
+//Her settes signalet opp
+func indefLoop() { 
+
+c := make(chan os.Signal, 1)
 signal.Notify(c,
 
 	syscall.SIGINT,)
-
+//En uendlig for loop som skriver "Loooooooooop" og sjekker etter syscall
 for {
 
 	fmt.Println("Loooooooooooooop")
