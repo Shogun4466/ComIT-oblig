9258152341b6dcf6d1845e265ade8cca94f5502b
Shogun4466 <35836530+Shogun4466@users.noreply.github.com>
2018-02-14 15:17:05 +0100

    Create Oppg-3.go

---
+++ Oblig-1/Oppg-3.go
@@ -0,0 +1,26 @@
+package main import ( "fmt"
+"os"
+
+"os/signal"
+
+"syscall"
+) func main() { indefLoop() } func indefLoop() { c := make(chan os.Signal, 1)
+signal.Notify(c,
+
+	syscall.SIGINT,)
+
+for {
+
+	fmt.Println("Loooooooooooooop")
+
+	a := <-c
+
+	switch a {
+
+	case syscall.SIGINT:
+
+		fmt.Println("You shall not pass SIGINT")
+	}
+	break
+}
+}
