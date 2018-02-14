13c60b04801e8c58a63a7ee54417f23dfd2b47f8
Vuong Nguyen <35836399+vuongnewguy@users.noreply.github.com>
2018-02-14 16:34:59 +0100

    Update iso_test.go

--- Oblig-1/iso_test.go
+++ Oblig-1/iso_test.go
@@ -3,6 +3,8 @@ package main
 import "testing"
 import "fmt"
 
+// Her konverterer vi heksadesimaler til string for å kunne putte verdiene inn i løkka. 
+
 func ExtendedASCIIText() string {
 	a:= []byte("\x80\x81\x82\x83\x84\x85\x86\x87\x88\x89\x8A\x8B\x8C\x8D\x8E\x8F\x90\x91\x92\x93\x94\x95\x96\x97\x98" +
 		"\x99\x9A\x9B\x9C\x9D\x9E\x9F\xA0\xA1\xA2\xA3\xA4\xA5\xA6\xA7\xA8\xA9\xAA\xAB\xAC\xAD\xAE\xAF\xB0\xB1\xB2\xB3" +
@@ -18,7 +20,8 @@ func TestExtendedASCII(t *testing.T) {
 	}
 }
 
-// Her forsøkte vi å definerer parametre for at c skal kun være innenfor rekkevide 128 og 255, men outputen blir ikke riktig
+/* Her forsøkte vi å definerer parametre for at c skal kun være innenfor rekkevide 128 og 255, 
+men outputen blir ikke riktig og dermed greier vi ikke å kjøre det riktig i terminalen*/
 
 func isExtendedASCII(s string) bool {
 	for c := range s {
