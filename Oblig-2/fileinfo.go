1fb98e08645c82512b11ff02e2791006e9405517
Vuong Nguyen <35836399+vuongnewguy@users.noreply.github.com>
2018-02-28 12:56:03 +0100

    eksempler og triks for oblig

--- Oblig-2/fileinfo.go
+++ Oblig-2/fileinfo.go
@@ -1,3 +1,5 @@
+// Oppgave 1A
+
 package main 
 
 import "fmt"
@@ -15,8 +17,72 @@ Is/Is not a device file
 Is/Is not a Unix character device
 Is/Is not a Unix block device
 Is/Is not a symbolic link
+
+Bruk av kode og eksempler fra GoLang kopieres og redigeres etter behov under
 */ 
 
-func (){
-  fmt.Println("Hei ")
+
+// golang for bruk av FileMode
+const (
+        // The single letters are the abbreviations
+        // used by the String method's formatting.
+        ModeDir        FileMode = 1 << (32 - 1 - iota) // d: is a directory
+        ModeAppend                                     // a: append-only
+        ModeExclusive                                  // l: exclusive use
+        ModeTemporary                                  // T: temporary file; Plan 9 only
+        ModeSymlink                                    // L: symbolic link
+        ModeDevice                                     // D: device file
+        ModeNamedPipe                                  // p: named pipe (FIFO)
+        ModeSocket                                     // S: Unix domain socket
+        ModeSetuid                                     // u: setuid
+        ModeSetgid                                     // g: setgid
+        ModeCharDevice                                 // c: Unix character device, when ModeDevice is set
+        ModeSticky                                     // t: sticky
+
+        // Mask for the type bits. For regular files, none will be set.
+        ModeType = ModeDir | ModeSymlink | ModeNamedPipe | ModeSocket | ModeDevice
+
+        ModePerm FileMode = 0777 // Unix permission bits
+)
+
+// eksempel på en funksjonsargument for oppgaven 
+func main() {
+	fi, err := os.Lstat("some-filename")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch mode := fi.Mode(); {
+	case mode.IsRegular():
+		fmt.Println("regular file")
+	case mode.IsDir():
+		fmt.Println("directory")
+	case mode&os.ModeSymlink != 0:
+		fmt.Println("symbolic link")
+	case mode&os.ModeNamedPipe != 0:
+		fmt.Println("named pipe")
+	}
 }
+
+/*
+
+Kode for å vise typer av file interface: 
+
+type FileInfo interface {
+        Name() string       // base name of the file
+        Size() int64        // length in bytes for regular files; system-dependent for others
+        Mode() FileMode     // file mode bits
+        ModTime() time.Time // modification time
+        IsDir() bool        // abbreviation for Mode().IsDir()
+        Sys() interface{}   // underlying data source (can return nil)
+}
+
+*/
+
+/* Oppgave 1B
+Må inneholde en directory som gjør at +build kan kjøre. Eksempel finner man på Golang-packages 
+
+*/
+
+
+
