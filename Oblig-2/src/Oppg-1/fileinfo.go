62c4d102e3d22a5868ae9b22aa0f842b5d958236
Rostrup <35837960+Rostrup@users.noreply.github.com>
2018-03-08 11:39:22 +0100

    Create fileinfo.go
    Komplett oppgave 1

---
+++ Oblig-2/src/Oppg-1/fileinfo.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main (){
+	FileInfo(file())
+}
+
+//Henter først ut argumentet fra kommandolinje så koden kan kjøres på alle filer eller mapper
+func file ()string{
+	arg := os.Args[1:]
+	var file string
+
+	for _, v := range arg {
+		file += v
+	}
+	return file
+}
+
+//Implementerer errorhåndtering og lesing av filen både for .os og FileInfo
+func FileInfo (file string) {
+
+	fi, err := os.Lstat(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	info, err := os.Stat(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	mode := fi.Mode()
+	fmt.Println ("Information about file",file,":")
+
+//oppretter variabler for å sjekke fakta om fil/mappe og størrelse
+	var bytes int64 = info.Size()
+	var kb float64 = (float64)(bytes/1024)
+	var mb float64 = (kb/1024)
+	var gb float64 = (mb/1024)
+
+fmt.Println("Size:",bytes,"in bytes,",kb,"KB,",mb,"MB and",gb,"GB")
+
+	if mode.IsDir () {
+		fmt.Println("Is a directory")
+	} else {
+		fmt.Println("Is not a directory")
+	}
+
+	if mode.IsRegular () {
+		fmt.Println("Is a regular file")
+	} else {
+		fmt.Println("Is not a regular file")
+	}
+
+	var unix = mode & os.ModePerm
+	if unix == 0777 {
+	fmt.Println("Has Unix permission bits: -rwxrwxrwx")
+	} else {
+	fmt.Println("Does not have Unix permission bits: -rwxrwxrwx")
+	}
+
+	var isappend = mode & os.ModeAppend
+	if isappend != 0 {
+		fmt.Println("Is append only")
+	} else {
+		fmt.Println("Is not append only")
+	}
+
+	var device = mode & os.ModeDevice
+	if device != 0 {
+		fmt.Println("Is a device file")
+	} else {
+		fmt.Println("Is not a device file")
+	}
+
+	var symbolic = mode & os.ModeSymlink
+	if symbolic != 0 {
+		fmt.Println("Is a symbolic link")
+	} else {
+		fmt.Println("Is not a symbolic link")
+	}
+}
