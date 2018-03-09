84d7c0fd5ddffc31244260e0b229615772222e40
Vuong Nguyen <35836399+vuongnewguy@users.noreply.github.com>
2018-03-09 13:33:43 +0100

    Update addup.go

--- Oblig-2/src/Oppg-3/addup.go
+++ Oblig-2/src/Oppg-3/addup.go
@@ -1,32 +1,22 @@
 package main
 
-
-
 import (
 	"os"
-
 	"strconv"
-
 	"fmt"
 )
 
-
-
 func main() {
-
 	input1 := os.Args[1]
-
 	input2 := os.Args[2]
 
 	tall1,_ := strconv.Atoi(input1)
 	tall2,_ := strconv.Atoi(input2)
 
 	s := make(chan int)
-
 	go noe(tall1, tall2, s)
-
 	sum := <-s
-
+	
 	fmt.Println(sum)
 }
 
