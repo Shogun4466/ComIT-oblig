02da79214039ec796e73c08a05f8db86e782a09f
Shogun4466 <35836530+Shogun4466@users.noreply.github.com>
2018-03-09 15:22:50 +0100

    Update addtofile.go

--- Oblig-2/src/Oppg-3/addtofile.go
+++ Oblig-2/src/Oppg-3/addtofile.go
@@ -1,16 +1,37 @@
 package main
 
-import "fmt"
-        "io"
-        "os"
-        "bufio"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
 func main() {
-    fi, err := os.Open("final.txt")
-    if err != nil {
-        panic(err)
-    }
- 
-        
+	file, err := os.Create("text.txt")
+	if err != nil {
+		log.Fatal("Cannot create file", err)
+	}
+	defer file.Close()
+
+
+	var number1 int
+
+	var number2 int
+
+
+
+	fmt.Println("Enter number: ")
+
+	fmt.Scan(&number1)
+
+	fmt.Println("Enter number to add to the first number: ")
+
+	fmt.Scan(&number2)
+
+	t := strconv.Itoa(number1)
+	n := strconv.Itoa(number2)
+
+	fmt.Fprintf(file, t)
+	fmt.Fprintf(file, n)
 }
-  
