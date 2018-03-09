4fa823e03794cb74689a2a4ac06aa07c1b2413a3
Vuong Nguyen <35836399+vuongnewguy@users.noreply.github.com>
2018-03-09 13:14:44 +0100

    Update addtofile.go

--- Oblig-2/src/Oppg-3/addtofile.go
+++ Oblig-2/src/Oppg-3/addtofile.go
@@ -1,7 +1,15 @@
 package main
 
 import "fmt"
+        "io"
+        "os"
+        "bufio"
 
-func main {
-
-}
+func main() {
+    // open input file
+    fi, err := os.Open("final.txt")
+    if err != nil {
+        panic(err)
+    }
+  
+  
