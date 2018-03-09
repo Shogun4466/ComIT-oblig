b447e881e5dfbfb9a1813ec606b0bb9c30ee98b7
Exscalibur <35968694+Exscalibur@users.noreply.github.com>
2018-03-09 17:13:10 +0100

    La til sleep i errorhåndteringa

--- Oblig-2/src/Oppg-3/addtofile.go
+++ Oblig-2/src/Oppg-3/addtofile.go
@@ -38,6 +38,7 @@ func writeToFile() {
 	intNumber1, err := strconv.Atoi(number1)
 	if err != nil{
 		fmt.Println("Please input a number. Shutting down.")
+		time.Sleep(2 * time.Second) //Trengs for at ledetekst vinduet ikke skal lukke seg med en gang
 		os.Exit(0)
 	}
 
@@ -46,6 +47,7 @@ func writeToFile() {
 	intNumber2, err := strconv.Atoi(number2)
 	if err != nil{
 		fmt.Println("Please input a number. Shutting down.")
+		time.Sleep(2 * time.Second) //Trengs for at ledetekst vinduet ikke skal lukke seg med en gang
 		os.Exit(0)
 	}
 
